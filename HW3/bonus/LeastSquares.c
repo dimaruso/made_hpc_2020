046acfab9d704becb2745a049fe7e8a7cf28eb6c
Dmitry <dimarussio@gmail.com>
2020-10-06 13:09:26 +0300

    add locker

--- HW3/bonus/LeastSquares.c
+++ HW3/bonus/LeastSquares.c
@@ -17,6 +17,8 @@ void seedThreads(const size_t nThreads, unsigned int* seeds) {
 
 int main(int argc, char* argv[])
 {
+    omp_lock_t locker;
+    omp_init_lock(&locker);
     const size_t nThreads = (argc > 2) ? atoi(argv[2]) : 4;
 
     srand(time(NULL));
@@ -24,11 +26,11 @@ int main(int argc, char* argv[])
     omp_set_num_threads(nThreads);
     seedThreads(nThreads, seeds);
 
-    double noise;
     const size_t noise_max = 3, X_max = 100; // noise & X limits for rand
+    double noise;
     size_t tid, seed;
+
     const double a = 17, b = 42; // real weight lin_reg
-    
     double a_lr = 0.0, b_lr = 0.0; // init weight lin_reg
     const double lr = 0.0003; // learning_rate
     double y_pred, d_y, d_x;
@@ -67,6 +69,8 @@ int main(int argc, char* argv[])
 #pragma omp for
         for (size_t i = 0; i < N; ++i)
         { 
+            omp_set_lock(&locker);
+            
             // forward
             y_pred = X[i] * a_lr + b_lr;
             // backward
@@ -75,12 +79,16 @@ int main(int argc, char* argv[])
             // update weight
             a_lr -= d_x * lr;
             b_lr -= d_y * lr;
-            //logger
-            if (i % (N / 20) == 0)
+
+            omp_unset_lock(&locker);
+            
+            // logger
+            if ((N < 20 && i % N == 0) || (N >= 20 && i % (N / 20) == 0))
                 printf("\ni = %d: y_pred = %f, y = %f, a = %f, b = %f", i, y_pred, y[i], a_lr, b_lr); 
         }
     }
     printf("\n\ntrue f(x) = %f * x + %f", a, b); 
     printf("\n\npred f(x) = %f * x + %f", a_lr, b_lr); 
+    omp_destroy_lock(&locker);
     return 0;
 }
